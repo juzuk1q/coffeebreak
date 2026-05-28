d00f472cbc278a6f1d280bb21596d01479307e17
juzuk1q <kurlykk6@gmail.com>
2026-05-29 00:14:16 +0500

    upd

--- lib/presentation/order/order_success_screen.dart
+++ lib/presentation/order/order_success_screen.dart
@@ -1,17 +1,65 @@
 import 'package:CoffeeBreak/core/constant/app_colors.dart';
 import 'package:CoffeeBreak/core/constant/text_styles.dart';
 import 'package:CoffeeBreak/core/widgets/app_button.dart';
+import 'package:CoffeeBreak/data/profile_service.dart';
 import 'package:CoffeeBreak/presentation/main_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:vize/vize.dart';
 
-class OrderSuccessScreen extends StatelessWidget {
+class OrderSuccessScreen extends StatefulWidget {
   final int total;
 
-  const OrderSuccessScreen({super.key, required this.total});
+  const OrderSuccessScreen({
+    super.key,
+    required this.total,
+  });
+
+  @override
+  State<OrderSuccessScreen> createState() => _OrderSuccessScreenState();
+}
+
+class _OrderSuccessScreenState extends State<OrderSuccessScreen> {
+  final _profileService = ProfileService();
+  String _orderNumber = '';
+  String _name = 'Пользователь';
+
+  Future<void> _loadOrderNumber() async {
+    final prefs = await SharedPreferences.getInstance();
+    int current = prefs.getInt('order_number') ?? 0;
+    current++;
+    await prefs.setInt('order_number', current);
+    setState(() {
+      _orderNumber = '#${current.toString().padLeft(3, '0')}';
+    });
+  }
+
+  Future<void> _loadProfile() async {
+    final profile = await _profileService.getProfile();
+
+    if (profile != null && mounted) {
+      setState(() {
+        _name = profile['name'] ?? 'Пользователь';
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProfile();
+    _loadOrderNumber();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final readyTime = now.add(const Duration(minutes: 10));
+
+    final formattedTime =
+        '${readyTime.hour.toString().padLeft(2, '0')}:'
+        '${readyTime.minute.toString().padLeft(2, '0')}';
+
     return Scaffold(
       backgroundColor: AppColor.white,
       body: Padding(
@@ -19,33 +67,48 @@ class OrderSuccessScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: .center,
           children: [
+            Spacer(),
             Container(
-              width: 120,
-              height: 120,
+              width: 177,
+              height: 177,
               decoration: BoxDecoration(
-                color: AppColor.green2,
-                shape: BoxShape.circle,
-              ),
-              child: Icon(
-                Icons.check_rounded,
-                color: AppColor.green1,
-                size: 64,
+                image: DecorationImage(image: AssetImage('assets/success.png'))
               ),
             ),
-            SizedBox(height: 32),
-            Text('Заказ оформлен!', style: TxtStyle.m18),
-            SizedBox(height: 12),
+            SizedBox(height: 8),
+
             Text(
-              'Ваш заказ на сумму $total ₽ успешно принят.',
-              style: TxtStyle.m14(color: AppColor.description),
-              textAlign: .center,
+              'Номер заказа $_orderNumber',
+              style: TxtStyle.m18,
             ),
-            SizedBox(height: 48),
+
+            SizedBox(height: 15),
+
+            Text(
+              '$_name, Ваш заказ успешно размещен.',
+              style: TxtStyle.m14(color: AppColor.description),
+              textAlign: TextAlign.center,
+            ),
+
+            SizedBox(height: 12),
+
+            Text(
+              'Заказ будет готов сегодня\n'
+                  'к $formattedTime по адресу\n'
+                  'г. Сургут, ул. Пушкина, д. 10',
+              style: TxtStyle.m14(),
+              textAlign: TextAlign.center,
+            ),
+
+            Spacer(),
+
             AppButton(
               text: 'Вернуться в меню',
               onTap: () => Navigator.pushAndRemoveUntil(
                 context,
-                MaterialPageRoute(builder: (_) => MainScreen()),
+                MaterialPageRoute(
+                  builder: (_) => MainScreen(),
+                ),
                     (route) => false,
               ),
             ),
